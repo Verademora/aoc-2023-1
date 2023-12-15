26f50b7b8ae804a00599f8ae7716ea43cc981dc8
Verademora <drhax123@gmail.com>
2023-12-15 02:56:00 -0600

    boilerplate to open and parse file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File; 
+use std::io::prelude::*;
+use std::error::Error; 
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Open our input file and read it to a String
+    let mut file = File::open("src/input")?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+    
+    let v: Vec<&str> = buffer.trim().split('\n').collect();
+
+    Ok(())
 }
